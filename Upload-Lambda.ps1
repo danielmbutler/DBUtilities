437c5c0e0f478ea66b5d638fbc3b1995b4478d1f
danielmbutler <46110343+danielmbutler@users.noreply.github.com>
2020-12-06 20:59:21 +0000

    Add files via upload

---
+++ Upload-Lambda.ps1
@@ -0,0 +1,12 @@
+﻿function Upload-LambdaFunction($Path, $DestinationPath, $Functionname){
+#Compress file
+
+Compress-Archive -Path $Path -DestinationPath $DestinationPath -CompressionLevel Fastest -Force
+
+aws lambda update-function-code --function-name $Functionname --zip-file fileb://$DestinationPath
+
+
+
+}
+
+Upload-LambdaFunction -Path C:\temp\connectme\Project\Backend\Description\* -DestinationPath C:\temp\connectme\Project\Backend\Description\function.zip -Functionname ThoughtDescription
